a1ed685249456d83ee8adbcff89d96a4d77b3d0c
Roman Protsyk <rprotsyk@gmail.com>
2020-08-04 13:02:59 +0300

    Initial version

---
+++ src/firebase/config.ts
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
